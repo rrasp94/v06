87275f582ec98862825bc7dcde01418bae725266
rrasp94 <ruraspud@vsite.hr>
2025-01-18 23:41:26 +0100

    v06 solved

--- app/app.h
+++ app/app.h
@@ -1,6 +1,39 @@
 #pragma once
 
-namespace vsite::oop::v6 
+namespace vsite::oop::v6
 {
+	template <typename T>
+	T minimum(T a, T b)
+	{
+		if (a < b) {
+			return a;
+		}
+		return b;
+	}
 
-}
+
+	template <typename T, int v>
+	class fixed_array
+	{
+	private:
+		T arr[v];
+
+	public:
+		size_t size()
+		{
+			return v;
+		}
+
+		T& operator [] (size_t index)
+		{
+			return arr[index];
+		}
+	};
+
+	template <typename T>
+	struct point
+	{
+		T a, b;
+	};
+
+};
